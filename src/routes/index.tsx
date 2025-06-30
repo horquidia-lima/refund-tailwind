99c2caca4f1f451066efd9a6b72f91103a004d07
Horquidia <horquidia.a.lima@gmail.com>
2025-06-30 16:13:12 -0300

    Componente de Loading

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,19 @@
 import { BrowserRouter } from "react-router";
 
 import { AuthRoutes } from "./AuthRoutes";
+import { Loading } from "../components/Loading";
 import { EmployeeRoutes } from "./EmployeeRoutes";
 import { ManagerRoutes } from "./ManagerRoutes";
 
+const isLoading = false
 export function Routes() {
+    if(isLoading){
+        return <Loading />
+    }
+
     return (
         <BrowserRouter>
-            <ManagerRoutes />
+            <AuthRoutes />
         </BrowserRouter>
     );
 }
