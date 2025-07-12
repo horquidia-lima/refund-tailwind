8370b6a0438b6610430d4836807d5dca1fc6b454
Horquidia <horquidia.a.lima@gmail.com>
2025-07-11 21:21:50 -0300

    Upload do arquivo

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -21,7 +21,7 @@ export function Refund() {
     const [amount, setAmount] = useState('')
     const [category, setCategory] = useState('')
     const [isloading, setIsLoading] = useState(false)
-    const [filename, setFilename] = useState<File | null>(null)
+    const [file, setFile] = useState<File | null>(null)
 
     const navigate = useNavigate()
     const params = useParams<{id: string}>()
@@ -36,6 +36,15 @@ export function Refund() {
         try {
             setIsLoading(true)
 
+            if(!file) {
+                return alert('Selecione um arquivo')
+            }
+
+            const fileUploadForm = new FormData()
+            fileUploadForm.append('file', file)
+
+            const response = await api.post("/uploads", fileUploadForm)
+
             const data = refundSchema.parse({
                 name,
                 amount: amount.replace(',', '.'),
@@ -44,7 +53,7 @@ export function Refund() {
 
             await api.post("/refunds", {
                 ...data,
-                filename: "1234568796325418963257.png"
+                filename: response.data.filename
             })
         
             navigate('/confirm', {state: {fromSubmit: true}})
@@ -109,8 +118,8 @@ export function Refund() {
                 </a>
             ): (
             <Upload
-                filename={filename && filename.name}
-                onChange={(e) => e.target.files && setFilename(e.target.files[0])}
+                filename={file && file.name}
+                onChange={(e) => e.target.files && setFile(e.target.files[0])}
             />)}
 
             <Button type="submit" isLoading={isloading}>
